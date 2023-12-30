1b1c31bd712d97ed712f2aaf939d1d18e75acb96
Parth Virani - Ocean M Tech <parthvirani.oceanmtech@gmail.com>
2023-12-30 09:36:40 +0530

    changes

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -40,21 +40,46 @@ class _MyAppState extends State<MyApp> {
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  ElevatedButton(
-                    onPressed: onPressed,
-                    style: ElevatedButton.styleFrom(padding: const EdgeInsets.symmetric(horizontal: 16)),
-                    child: const Text("From Camera"),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: onPressed,
+                      style: ElevatedButton.styleFrom(padding: const EdgeInsets.symmetric(horizontal: 16)),
+                      child: const Text("From Camera"),
+                    ),
                   ),
-                  ElevatedButton(
-                    onPressed: onPressed,
-                    style: ElevatedButton.styleFrom(padding: const EdgeInsets.symmetric(horizontal: 16)),
-                    child: const Text("From Gallery"),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: onPressed,
+                      style: ElevatedButton.styleFrom(padding: const EdgeInsets.symmetric(horizontal: 16)),
+                      child: const Text("From Gallery"),
+                    ),
                   ),
-                  ElevatedButton(
-                    onPressed: onPressed,
-                    style: ElevatedButton.styleFrom(padding: const EdgeInsets.symmetric(horizontal: 16)),
-                    child: const Text("Create PDF"),
+                  const SizedBox(width: 16),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: onPressed,
+                      style: ElevatedButton.styleFrom(padding: const EdgeInsets.symmetric(horizontal: 16)),
+                      child: const Text("Bulk Scan"),
+                    ),
                   ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: onPressed,
+                      style: ElevatedButton.styleFrom(padding: const EdgeInsets.symmetric(horizontal: 16)),
+                      child: const Text("Create PDF"),
+                    ),
+                  ),
+                  const SizedBox(width: 16),
                 ],
               ),
               for (var picture in _pictures) Image.file(File(picture)),
@@ -68,7 +93,7 @@ class _MyAppState extends State<MyApp> {
   void onPressed() async {
     List<String> pictures;
     try {
-      pictures = await CunningDocumentScanner.getPictures(true) ?? [];
+      pictures = await CunningDocumentScanner.getPictures(crop: true, imagesPath: []) ?? [];
       if (!mounted) return;
       setState(() {
         _pictures = pictures;
--- lib/cunning_document_scanner.dart
+++ lib/cunning_document_scanner.dart
@@ -1,24 +1,30 @@
 import 'dart:async';
 
 import 'package:flutter/services.dart';
-import 'package:permission_handler/permission_handler.dart';
-
-enum Source { CAMERA, GALLERY }
 
 class CunningDocumentScanner {
   static const MethodChannel _channel = MethodChannel('cunning_document_scanner');
 
   /// Call this to start get Picture workflow.
-  static Future<List<String>?> getPictures(bool crop) async {
-    Map<Permission, PermissionStatus> statuses = await [
-      Permission.camera,
-      Permission.storage,
-    ].request();
-    if (statuses.containsValue(PermissionStatus.denied) || statuses.containsValue(PermissionStatus.permanentlyDenied)) {
-      throw Exception("Permission not granted");
-    }
+  static Future<List<String>?> getPictures({required List<String> imagesPath, required bool crop}) async {
+    // Map<Permission, PermissionStatus> statuses = await [
+    //   Permission.camera,
+    //   Permission.storage,
+    //   Permission.photos,
+    // ].request();
+    // if (statuses.containsValue(PermissionStatus.denied) || statuses.containsValue(PermissionStatus.permanentlyDenied)) {
+    //   throw Exception("Permission not granted");
+    // }
 
-    final List<dynamic>? pictures = await _channel.invokeMethod('getPictures', {'crop': crop});
+    // Check permission from flutter level
+
+    final List<dynamic>? pictures = await _channel.invokeMethod(
+      'getPictures',
+      {
+        "crop": crop,
+        "imagesPath": imagesPath,
+      },
+    );
     return pictures?.map((e) => e as String).toList();
   }
 }
